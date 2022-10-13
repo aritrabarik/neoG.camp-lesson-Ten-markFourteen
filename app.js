334d43b420ff6deaa4c9143ea96d7c2d75f87bef
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-10-13 21:13:07 +0530

    logic building done till ex04

---
+++ app.js
@@ -0,0 +1,11 @@
+// ex03-04: breaking the problem into pieces
+
+var initialPrice = document.querySelector("#initial-price");
+
+var currentPrice = document.querySelector("#curret-price");
+
+var stocksQuantity = document.querySelector("#stocks-quantity");
+
+var submitBtn = document.querySelector("#submit-btn");
+
+var outputBox = document.querySelector("#output-box");
